80b8491a02bad9e9eaac1ad6285c176d248fa4dd
Mohit Agarwal <mohit.agarwal@coindcx.com>
2023-09-19 10:37:13 +0530

    root route change

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'transactions/:entity_id', to: 'transactions#index', as: :entity_transactions
+  
+  root to: "transactions/76510190788"
 end
